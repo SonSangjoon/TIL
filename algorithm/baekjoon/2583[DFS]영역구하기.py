a6c4518ab2c776b77ea9dd2a08ff7f1025b285d6
onshore <onshorecoding9@gmail.com>
2021-06-21 21:04:50 +0900

    210621: algorithm baekjoon

---
+++ algorithm/baekjoon/2583[DFS]영역구하기.py
@@ -0,0 +1,38 @@
+def dfs(x, y):
+    global M, N
+    cnt = 0
+    visited[x][y] = 1
+    dx = [-1, 0, 1, 0]
+    dy = [0, -1, 0, 1]
+
+    for i in range(4):
+        new_x = x + dx[i]
+        new_y = y + dy[i]
+        if 0 <= new_x < M and 0 <= new_y < N:
+            if visited[new_x][new_y] == 0 and default_mp[new_x][new_y] == 0:
+                dfs(new_x, new_y)
+                cnt += 1
+                return cnt
+    return cnt
+
+M, N, n = map(int, input().split())
+mp = [list(map(int, input().split())) for _ in range(n)]
+default_mp = [[0] * N for _ in range(M)]
+visited = [[0] * N for _ in range(M)]
+answer = []
+cnt = 0
+
+for r in mp:
+    a, b, c, d = r[0], r[1], r[2], r[3]
+    for i in range(c, d):
+        for j in range(a, b):
+            default_mp[i][j] = 1
+
+for i in range(M):
+    for j in range(N):
+        if default_mp[i][j] == 0 and visited[i][j] == 0:
+            ans = dfs(i, j)
+            if ans != 0:
+                answer.append(ans)
+
+print(answer)
---
+++ algorithm/baekjoon/4963[DFS]섬의개수.py
@@ -0,0 +1,35 @@
+import sys
+sys.setrecursionlimit(10000000)
+
+def dfs(x,y):
+    dx = [-1,-1,0,0,1,-1,1,1]
+    dy = [-1,1,-1,1,0,0,-1,1]
+    visited[x][y] = 1
+    for i in range(8):
+
+        new_x = x + dx[i]
+        new_y = y + dy[i]
+        if 0<= new_x< h and 0<= new_y< w:
+            if visited[new_x][new_y] == 0 and mp[new_x][new_y] == 1:
+                dfs(new_x, new_y)
+
+answer = []
+
+while True:
+    w, h = map(int,input().split())
+
+    if w == 0 and h == 0:
+        break 
+
+    mp = [list(map(int, input().split())) for _ in range(h) ]
+    visited = [[0]*w for _ in range(h)]
+    ans = 0
+
+    for i in range(h):
+        for j in range(w):
+            if visited[i][j] == 0 and mp[i][j] == 1:
+                ans += 1
+                dfs(i,j)
+    answer.append(str(ans))
+
+print("\n".join(answer))
