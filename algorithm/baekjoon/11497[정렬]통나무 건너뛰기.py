ea3e637f2393bb5f1f83e9e571e472a7dc45aed2
onshore <onshorecoding9@gmail.com>
2021-07-06 16:20:02 +0900

    210706: algorithm baekjoon

---
+++ algorithm/baekjoon/11497[정렬]통나무 건너뛰기.py
@@ -0,0 +1,14 @@
+T = int(input())
+ans = []
+for _ in range(T):
+    n = int(input())
+    lst = list(map(int, input().split()))
+    lst.sort()
+    max_level = 0
+    for i in range(2,n):
+        l = lst[i]-lst[i-2]
+        if l > max_level:
+            max_level = l
+    ans.append(str(max_level))
+
+print("\n".join(ans))
---
+++ algorithm/baekjoon/2866[이분탐색]문자열 잘라내기.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+R, C = map(int, input().split())
+
+row_lst = [list(input()) for _ in range(R)]
+
+column_lst = list(map(list, zip(*row_lst)))
+s, e = 1, R
+ch  = False
+while s < e:
+    m = (s+e)//2 
+    checker = []
+    for letter in column_lst:
+        l = letter[m:] 
+        if l in checker:
+            e = m-1
+            ch = True
+            continue
+        else:
+            checker.append(l)
+    s = m + 1
+
+if ch:
+    print(e)
+else:
+    print(e+1)
