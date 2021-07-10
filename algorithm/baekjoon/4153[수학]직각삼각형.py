0bb7206abd530d00f693bb8f0f005884bc609d46
onshore <onshorecoding9@gmail.com>
2021-07-10 21:33:07 +0900

    210710: algorithm baekjoon

---
+++ algorithm/baekjoon/4153[수학]직각삼각형.py
@@ -0,0 +1,16 @@
+lst = []
+while True:
+    l = list(map(int, input().split()))
+
+    if not sum(l):
+        break
+    lst.append(l)
+
+
+for sides in lst:
+
+    sides.sort()
+    if sides[0]**2 + sides[1]**2 == sides[2]**2:
+        print("right")
+    else:
+        print("wrong")
