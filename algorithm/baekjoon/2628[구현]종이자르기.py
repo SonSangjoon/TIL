bacbb51e29642819c06e5e100e5e35c0cda4dbc2
onshore <onshorecoding9@gmail.com>
2021-08-20 21:09:19 +0900

    210820: algorithm baekjoon s5

---
+++ algorithm/baekjoon/2628[구현]종이자르기.py
@@ -0,0 +1,22 @@
+a, b = map(int, input().split())
+n = int(input())
+lst = [tuple(map(int, input().split())) for _ in range(n)]
+
+
+lst.append((1, a))
+lst.append((0, b))
+lst.sort(key=lambda x: (x[0], x[1]))
+
+max_row, prev_row = 0, 0
+max_col, prev_col = 0, 0
+
+for idx, cut in lst:
+    if idx:
+        l = cut - prev_row
+        max_row, prev_row = max(max_row, l), cut
+    else:
+        l = cut - prev_col
+        max_col, prev_col = max(max_col, l), cut
+
+ans = max_row * max_col
+print(ans)
